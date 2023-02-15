e035b74ece18b21fc54f269f8ef47a955b81a608
Dinara <dinaranav1987@gmail.com>
2023-02-15 15:10:45 +0400

    feat: add auth middleware to router

--- router/router.js
+++ router/router.js
@@ -9,7 +9,7 @@ import { body } from "express-validator";
 import authMiddleware from "../middleware/authMiddleware.js";
 import roleMiddleware from "../middleware/roleMiddleware.js";
 
-router.post('/users', [
+router.post('/users', authMiddleware(), roleMiddleware(['admin', 'manager']), [
   body('data.mail', 'Email is required').notEmpty(),
   body('data.mail', 'Email must be correct email type').isEmail(),
   body('data.password', 'Password must have 8 symbols or more').isLength({ min: 8 }),
@@ -21,16 +21,14 @@ router.post('/users', [
   body('role', 'Role is required').notEmpty(),
 ], usersController.addUser);
 
-router.patch('/users/:id', [
+router.patch('/users/:id', authMiddleware(), roleMiddleware(['admin', 'manager']), [
   body('data.mail', 'Email must be correct email type').if(body('data.mail').notEmpty()).isEmail(),
   body('data.password', 'Password must have 8 symbols or more').if(body('data.password').notEmpty()).isLength({ min: 8 }),
 ], usersController.updateUser);
 
-router.delete('/users/:id', usersController.deleteUser);
-router.patch('/users/archived/:id', usersController.undeleteUser);
-
-// router.get('/users', authMiddleware(), roleMiddleware(['admin', 'manager']), usersController.getUsers);
-router.get('/users', usersController.getUsers);
+router.delete('/users/:id', authMiddleware(), roleMiddleware(['admin', 'manager']), usersController.deleteUser);
+router.patch('/users/archived/:id', authMiddleware(), roleMiddleware(['admin', 'manager']), usersController.undeleteUser);
+router.get('/users', authMiddleware(), roleMiddleware(['admin', 'manager']), usersController.getUsers);
 
 router.get('/profile', authMiddleware(), usersController.getProfile);
 router.patch('/profile', authMiddleware(), [
@@ -42,28 +40,28 @@ router.post('/auth/login', authController.login);
 router.post('/auth/logout', authController.logout);
 router.get('/auth/refresh', authController.refresh);
 
-router.post('/companies', [
+router.post('/companies', authMiddleware(), [
   body('data.companyName', 'Company name is required').notEmpty(),
+  body('data.commonPhone', 'Phone is required').notEmpty(),
   // body('contacts.commonPhone', 'Company phones should be passed as an array').isArray(),
   // body('contacts.workers', 'Contacts should be passed as an array').isArray(),
   // body('contacts.workers.*.phone', 'Contacts\' phones should be passed as an array').isArray(),
   // body('users', 'Users\' ids should be passed as an array').isArray(),
 ], companiesController.addCompany);
 
-router.patch('/companies/:id', companiesController.updateCompany);
+router.patch('/companies/:id', authMiddleware(), companiesController.updateCompany);
 
-router.delete('/companies/:id', companiesController.deleteCompany);
-router.patch('/companies/archived/:id', companiesController.undeleteCompany);
+router.delete('/companies/:id', authMiddleware(), roleMiddleware(['admin', 'manager']), companiesController.deleteCompany);
+router.patch('/companies/archived/:id', authMiddleware(), roleMiddleware(['admin', 'manager']), companiesController.undeleteCompany);
 
 router.get('/companies', authMiddleware(), companiesController.getCompanies);
-// router.get('/companies/archived', companiesController.getArchivedCompanies);
 
-router.post('/clients', clientsController.addClient);
-router.patch('/clients/:id', clientsController.updateClient);
-router.delete('/clients/:id', clientsController.deleteClient);
+router.post('/clients', authMiddleware(), clientsController.addClient);
+router.patch('/clients/:id', authMiddleware(), clientsController.updateClient);
+router.delete('/clients/:id', authMiddleware(), clientsController.deleteClient);
 router.get('/clients', authMiddleware(), clientsController.getClients);
 
-router.post('/todos', [
+router.post('/todos', authMiddleware(), [
   body('data.type', 'Todo type is required').notEmpty(),
   body('data.startTime', 'Start time is required').notEmpty(),
   body('data.endTime', 'End time is required').notEmpty(),
@@ -72,10 +70,10 @@ router.post('/todos', [
   body('company', 'Company is required').notEmpty(),
 ], todosController.addTodo);
 
-router.patch('/todos/:id', todosController.updateTodo);
+router.patch('/todos/:id', authMiddleware(), todosController.updateTodo);
 
 // router.delete('/todos/:id', todosController.deleteTodo);
 
-router.get('/todos', authMiddleware(), todosController.getTodos);
+router.get('/todos', authMiddleware(), authMiddleware(), todosController.getTodos);
 
 export default router;
