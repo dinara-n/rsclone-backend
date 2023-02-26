74c3fe9388b44ceae6306fd9a84431af2139b4b9
Dinara <dinaranav1987@gmail.com>
2023-02-26 16:15:05 +0400

    fix: make minor fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   credentials: true,
-  origin: [process.env.CLIENT_LOCAL_URL, process.env.CLIENT_DEPLOY_URL],
+  origin: true,
 }));
 app.use(cookieParser());
 app.use('/', router);
